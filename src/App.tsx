55418a4350f6952020533c59b89ef48ee64d7c4b
Wojciech Malarski <w.malarski@gmail.com>
2022-12-03 22:32:51 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { ReactElement, useState } from "react";
+
+export const App = (): ReactElement => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
